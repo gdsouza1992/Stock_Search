e363edc5bf8342f60ce51c9dd8e769073a949202
Gareth Dsouza <garethdsouza@Gareths-MacBook-Air.local>
2016-03-13 14:43:38 -0700

    Updated the Historical Data API call string

--- php/stocks.php
+++ php/stocks.php
@@ -2,18 +2,54 @@
     function getStockInfo($stockName){
         if($stockName == null)
         {
-            define('SEARCH_STOCK_URL','http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol=AAPL');
-            return file_get_contents(SEARCH_STOCK_URL);    
+            $apiURL = 'http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol=AAPL';
+            return file_get_contents($apiURL);    
         }else{
-            define('SEARCH_STOCK_URL','http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol='.$stockName);
-            return file_get_contents(SEARCH_STOCK_URL);       
+            $apiURL = 'http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol='.$stockName;
+            return file_get_contents($apiURL);       
         }
     }
+
+    function getStockChartImage($stockName){
+        if($stockName == null)
+        {
+            return "No chart data fond for ".$stockName;
+        }else{
+            $apiURL = 'http://chart.finance.yahoo.com/t?s='.$stockName.'&lang=en-US&width=500&height=400';
+            $data = file_get_contents($apiURL);
+            if($data != null)
+            {
+                //file exists at url
+                return "<img src='".$apiURL."' alt='Graph for stock - ".$stockName."' />"    ;
+                
+            }else{
+                return "No chart data fond for ".$stockName;
+            }
+        }
+    }
+
+    function getStockHistoricalData($stockName,$days){
+        $apiURL = "http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json?parameters={'Normalized':false,'NumberOfDays':".$days.",'DataPeriod':'Day','Elements':[{'Symbol':'".$stockName."','Type':'price','Params':['ohlc']}]}";
+        var_dump(file_get_contents($apiURL));    
+    }
+
     
-    if($_GET['action'] == "stockInfo")
+    
+    if(isset($_GET['action']) && $_GET['action'] == "stockInfo")
     {
         echo getStockInfo($_GET['param']);
     }
+    if(isset($_GET['action']) && $_GET['action'] == "stockChart")
+    {
+        echo getStockChartImage($_GET['param']);
+    }
+    if(isset($_GET['action']) && $_GET['action'] == "stockHistory")
+    {
+        $stockName = $_GET['param'];
+        $days = $_GET['since'];
+        echo getStockHistoricalData($stockName,$days);
+    }
+    
 
     
 ?>
