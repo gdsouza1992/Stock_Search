7dcf95c671809fbb6b9efda82b047c00df1ee31f
Gareth Dsouza <garethdsouza@Gareths-MacBook-Air.local>
2016-03-13 21:46:25 -0700

    Historical data is getting generated.

--- script/script.js
+++ script/script.js
@@ -7,8 +7,6 @@ function getStockDataAsync(stock_symbol){
         data: {'action': 'stockInfo','param':stock_symbol}
     });
 }
-
-//http://chart.finance.yahoo.com/t?s=M&lang=en-US&width=400&height=300
 function getStockChartAsync(stock_symbol){
     return $.ajax({
         dataType:"text",
@@ -69,15 +67,19 @@ function searchStockChart(stock_symbol){
     });
 }
 
+var g;
 
 function setHistoricalChart(stock_symbol){
     var htmlStr;
-    var params = getInputParams(stock_symbol,3);
+    var params = getInputParams(stock_symbol,2554);
     var paramsJsonString = JSON.stringify(params);
     var promise = getStockHistAsync(paramsJsonString);
     promise.success(function(data){
-        htmlStr = data;
-        $('#historicalChart').append(htmlStr);
+        jsonData = JSON.parse(data);
+        var stock_symbol = jsonData.Elements[0].Symbol
+        //g= jsonData;
+        Markit_render(jsonData,stock_symbol);
+        //$('#historicalChart').append(htmlStr);
     });
 }
 
@@ -90,7 +92,7 @@ getInputParams = function(_symbol,_duration){
             {
                 Symbol: _symbol,
                 Type: "price",
-                Params: ["ohlc"] //ohlc, c = close only
+                Params: ["c"] //ohlc, c = close only
             },
             {
                 Symbol: _symbol,
@@ -100,6 +102,77 @@ getInputParams = function(_symbol,_duration){
     }
 };
 
-searchStockTable("MSFT");
-searchStockChart("MSFT");
-setHistoricalChart("GOOG");
+//Markit API - Edited from Github
+Markit_fixDate = function(dateIn) {
+    var dat = new Date(dateIn);
+    return Date.UTC(dat.getFullYear(), dat.getMonth(), dat.getDate());
+};
+
+Markit_getValue = function(json) {
+    var dates = json.Dates || [];
+    var elements = json.Elements || [];
+    var chartSeries = [];
+
+    if (elements[0]){
+
+        for (var i = 0, datLen = dates.length; i < datLen; i++) {
+            var dat = Markit_fixDate( dates[i] );
+            var pointData = [
+                dat,
+                //elements[0].DataSeries['open'].values[i],
+                //elements[0].DataSeries['high'].values[i],
+                //elements[0].DataSeries['low'].values[i],
+                elements[0].DataSeries['close'].values[i]
+            ];
+            chartSeries.push( pointData );
+        };
+    }
+    return chartSeries;
+};
+
+Markit_render = function(data,stock_symbol) {
+    
+    var values = Markit_getValue(data);
+    //volume = Markit_getVolume(data);  
+    //jsonDataV =volume;
+    //jsonData = ohlc;
+    
+    $('#historicalChart').highcharts('StockChart', {
+
+
+            rangeSelector : {
+                selected : 1
+            },
+
+            title : {
+                text : stock_symbol+' Stock Price'
+            },
+
+            series : [{
+                name : stock_symbol+' Stock Price',
+                data : values,
+                type : 'area',
+                threshold : null,
+                tooltip : {
+                    valueDecimals : 2
+                },
+                fillColor : {
+                    linearGradient : {
+                        x1: 0,
+                        y1: 0,
+                        x2: 0,
+                        y2: 1
+                    },
+                    stops : [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                    ]
+                }
+            }]
+        });    
+};
+
+var stock_symbol = "GOOG";
+searchStockTable(stock_symbol);
+searchStockChart(stock_symbol);
+setHistoricalChart(stock_symbol);
